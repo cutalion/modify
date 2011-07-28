e02642c66d43dbe76ae65695144aaf6ddbeef0d6
Alexander Glushkov <cutalion@gmail.com>
2011-07-28 16:38:56 +0300

    first version - no tests, just for fun

---
+++ lib/modify.rb
@@ -0,0 +1,24 @@
+module Modify  
+  def self.included(base)  
+    base.extend ClassMethods  
+  end  
+
+  module ClassMethods  
+
+    def modify(*fiels, &block)  
+      before_validation do |record|
+        fields.each do |field|
+          old_value = record.send(field)
+          new_value = block.call old_value
+
+          record.send( "#{field}=", new_value )
+        end  
+      end
+    end  
+
+  end  
+end  
+
+class ActiveRecord::Base  
+  include Modify  
+end  
